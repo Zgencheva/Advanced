2673601d927f8b6895d923390a46a6d9c7161053
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-10-16 16:26:55 +0300

    08. Balanced Parenthesis

--- StacksAndQueues/08.BalancedParenthesis/Program.cs
+++ StacksAndQueues/08.BalancedParenthesis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08.BalancedParenthesis
 {
@@ -6,7 +7,65 @@ namespace _08.BalancedParenthesis
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine(); //{[()]} ()()()()()() //()((()))[][][][(((())))]
+            Stack<char> stack = new Stack<char>();
+            bool areTheyEqual = true;
+            if (input.Length % 2 != 0)
+            {
+                areTheyEqual = false;
+            }
+            else
+            {
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == '(' || input[i] == '[' || input[i] == '{')
+                    {
+                        stack.Push(input[i]);
+                    }
+                    else if (input[i] == ')' || input[i] == ']' || input[i] == '}')
+                    {
+                        if (stack.Count == 0)
+                        {
+                            areTheyEqual = false;
+                            break;
+                        }
+                        char currentPart = stack.Pop();
+                        if (currentPart == '(')
+                        {
+                            if (input[i] != ')')
+                            {
+                                areTheyEqual = false;
+                                break;
+                            }
+                        }
+                        else if (currentPart == '[')
+                        {
+                            if (input[i] != ']')
+                            {
+                                areTheyEqual = false;
+                                break;
+                            }
+                        }
+                        else if (currentPart == '{')
+                        {
+                            if (input[i] != '}')
+                            {
+                                areTheyEqual = false;
+                                break;
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            if (stack.Count == 0 && areTheyEqual)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
         }
     }
 }
---
+++ StacksAndQueues/9.SimpleTestEditor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9.SimpleTestEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+
+            }
+        }
+    }
+}
