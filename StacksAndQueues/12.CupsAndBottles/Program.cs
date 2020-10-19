9369377aee3d095db47ff8ae5140102691cadf7d
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-10-19 16:56:20 +0300

    12. Cups and Bottles

--- StacksAndQueues/12.CupsAndBottles/Program.cs
+++ StacksAndQueues/12.CupsAndBottles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _12.CupsAndBottles
 {
@@ -6,7 +8,48 @@ namespace _12.CupsAndBottles
     {
         static void Main(string[] args)
         {
-            
+            int[] caps = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] bottles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> capsCapacity = new Queue<int>(caps);
+            Stack<int> bottlesCapacity = new Stack<int>(bottles);
+            int wastedWater = 0;
+            bool isTheCapFull = true;
+            int currentCap = 0;
+            while (bottlesCapacity.Count > 0)
+            {
+                int currentBottle = bottlesCapacity.Pop();
+                if (isTheCapFull)
+                {
+                    currentCap = capsCapacity.Peek();
+                }
+                
+                if (currentBottle >= currentCap)
+                {
+                    capsCapacity.Dequeue();
+                    isTheCapFull = true;
+                    wastedWater += currentBottle - currentCap;
+                }
+                else
+                {
+                    currentCap -= currentBottle;
+                    isTheCapFull = false;
+
+                }
+                if (capsCapacity.Count == 0)
+                {
+                    break;
+                }
+            }
+            if (bottlesCapacity.Count == 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", capsCapacity)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
+            else if (capsCapacity.Count == 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottlesCapacity)}");
+                Console.WriteLine($"Wasted litters of water: {wastedWater}");
+            }
         }
     }
 }
