8c6617fe51d791434098d5c057f59f0fd8990f65
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-11-08 16:14:30 +0200

    new

--- MultidimensionalArrays/7.PascalTriangle/Program.cs
+++ MultidimensionalArrays/7.PascalTriangle/Program.cs
@@ -6,7 +6,37 @@ namespace _7.PascalTriangle
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int cols = 1;
+            long[][] pascal = new long[n][];
+            for (int row = 0; row < n; row++)
+            {
+                pascal[row] = new long[cols];
+                pascal[row][0] = 1;
+                pascal[row][pascal[row].Length - 1] = 1;
+                if (row > 1)
+                {
+                    for (int col = 1; col < pascal[row].Length - 1; col++)
+                    {
+                        long[] previousRow = pascal[row - 1];
+                        long firstNum = previousRow[col];
+                        long secondNum = previousRow[col - 1];
+                        pascal[row][col] = firstNum + secondNum;
+
+                    }
+                }
+                cols++;
+
+            }
+            for (int row = 0; row < pascal.Length; row++)
+            {
+                for (int col = 0; col < pascal[row].Length; col++)
+                {
+                    Console.Write(pascal[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
         }
+                
     }
 }
---
+++ MultidimensionalArrays/SpiralMatrix/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+
+namespace SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            string direction = "right";
+            int row = 0;
+            int col = 0;
+            for (int i = 0; i < n * n; i++)
+            {
+                matrix[row, col] = i + 1;
+                if (direction == "right")
+                {
+
+                    col++;
+
+                    if (col == n || matrix[row, col] != 0)
+                    {
+                        col--;
+                        direction = "down";
+
+                    }
+                }
+                if (direction == "down")
+                {
+                    row++;
+                    if (row == n || matrix[row, col] != 0)
+                    {
+                        row--;
+                        direction = "left";
+                    }
+                }
+                if (direction == "left")
+                {
+                    col--;
+                    if (col == -1 || matrix[row, col] != 0)
+                    {
+                        col++;
+                        direction = "up";
+                    }
+                }
+                if (direction == "up")
+                {
+                    row--;
+                    if (row == -1 || matrix[row,col] != 0)
+                    {
+                        row++;
+                        col++;
+                        direction = "right";
+                    }
+                }
+                
+            }
+
+            GetMatrix(matrix);
+        }
+
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        static void GetMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
