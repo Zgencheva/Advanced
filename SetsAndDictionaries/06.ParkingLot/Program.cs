1d7d44af750606c99b5808e263d419d5c444c01a
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-11-24 14:33:03 +0200

    LabSets

---
+++ SetsAndDictionaries/06.ParkingLot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                if (command[0] == "END")
+                {
+                    break;
+                }
+                string direction = command[0];
+                string carNum = command[1];
+                if (direction == "IN")
+                {
+                    parkingLot.Add(carNum);
+                }
+                else if (direction == "OUT")
+                {
+                    parkingLot.Remove(carNum);
+                }
+            }
+
+            if (parkingLot.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var item in parkingLot)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
---
+++ SetsAndDictionaries/07.SoftuniParty/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace _07.SoftuniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> guests = new HashSet<string>();
+            while (true)
+            {
+                string guest = Console.ReadLine();
+                if (guest == "PARTY")
+                {
+                    break;
+                }
+                guests.Add(guest);
+            }
+            while (true)
+            {
+                string guest = Console.ReadLine();
+                if (guest == "END")
+                {
+                    break;
+                }
+                guests.Remove(guest);
+            }
+
+            Console.WriteLine(guests.Count);
+            foreach (var item in guests)
+            {
+                if (item.StartsWith('1') || item.StartsWith('2') || item.StartsWith('3') || item.StartsWith('4')
+                    || item.StartsWith('5') || item.StartsWith('6') || item.StartsWith('7') || item.StartsWith('8')
+                    || item.StartsWith('9') || item.StartsWith('0'))
+                {
+                    Console.WriteLine(item);
+                    
+                }
+               
+            }
+            foreach (var item in guests)
+            {
+                if (!(item.StartsWith('1') || item.StartsWith('2') || item.StartsWith('3') || item.StartsWith('4')
+                    || item.StartsWith('5') || item.StartsWith('6') || item.StartsWith('7') || item.StartsWith('8')
+                    || item.StartsWith('9') || item.StartsWith('0')))
+                {
+                    Console.WriteLine(item);
+
+                }
+
+            }
+        }
+    }
+}
