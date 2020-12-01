91ab28078effa3ae0dfc7642647ed5f6bcb2065d
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-01 20:20:56 +0200

    StreamsEx

---
+++ StreamsFilesDirectories/01.EvenLines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    string line = reader.ReadLine();
+                    int counter = 0;
+                    while (line != null)
+                    {
+                        if (counter % 2 == 0)
+                        {
+                            Regex pattern = new Regex("[-,,,.,!,?]");
+                            line = pattern.Replace(line, "@");
+                            var array = line.Split().ToArray().Reverse();
+                            writer.WriteLine(string.Join(" ", array));
+                            Console.WriteLine(string.Join(" ", array));
+                        }
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ StreamsFilesDirectories/02.LineNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../text.txt");
+            string[] result = new string[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string currentLine = lines[i];
+                int countLetters = CountAllLetters(currentLine);
+                int countMarks = CountAllPunctuationMarks(currentLine);
+                result[i] = $"Line {i + 1}: {lines[i]} ({countLetters})({countMarks})";
+
+            }
+
+            File.WriteAllLines("../../../output.txt", result);
+
+        }
+
+        static int CountAllLetters(string line)
+        {
+            int count = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (char.IsLetter(line[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;    
+        }
+
+        static int CountAllPunctuationMarks(string line)
+        {
+            int count = 0;
+            char[] punctuationMarks = new char[] { '-', ',', '.', '!', '?', ';' };
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (punctuationMarks.Contains(line[i]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
