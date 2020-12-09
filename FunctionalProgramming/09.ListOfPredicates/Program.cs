472951b0bbafc29e7bcccf13b4223c819e0cbd0b
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-09 21:42:47 +0200

    FuncP

---
+++ FunctionalProgramming/09.ListOfPredicates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).Distinct().ToList();
+
+            Console.WriteLine(string.Join(" ", GetDivisibleNums(n, nums)));
+           
+        }
+
+        static List<int> GetDivisibleNums(int n, List<int> divisions)
+        {
+            List<int> output = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Func<int, bool> predicate = x => i % x != 0;
+                bool isDivisble = true;
+                foreach (var item in divisions)
+                {
+                    if (predicate(item))
+                    {
+                        isDivisble = false;
+                        break;
+                    }
+                }
+                if (isDivisble)
+                {
+                    output.Add(i);
+                }
+            }
+
+            return output;
+
+        }
+
+
+    }
+}
---
+++ FunctionalProgramming/10.PredicateParty/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _10.PredicateParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> partyPeople = Console.ReadLine().Split().ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                
+                if (command == "Party!")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split().ToArray();
+                Predicate<string> predicateStartsWith = x => x.StartsWith(tokens[2]);
+                Predicate<string> predicateEndsWith = x => x.EndsWith(tokens[2]);
+                Predicate<string> predicateNameLength = x => x.Length == int.Parse(tokens[2]);
+
+                
+                if (tokens[0] == "Remove")
+                {
+                    if (tokens[1] == "StartsWith")
+                    {
+                        partyPeople.RemoveAll(predicateStartsWith);
+                    }
+                    else if (tokens[1] == "EndsWith")
+                    {
+                        partyPeople.RemoveAll(predicateEndsWith);
+                    }
+                    else if (tokens[1] == "Length")
+                    {
+                        partyPeople.RemoveAll(predicateNameLength);
+                    }
+                }
+                if (tokens[0] == "Double")
+                {
+                    string nameToaAdd = null;
+                    int times = 0;
+                    if (tokens[1] == "StartsWith")
+                    {
+                        foreach (var item in partyPeople)
+                        {
+                            if (predicateStartsWith(item))
+                            {
+                                nameToaAdd = item;
+                                times++;
+                            }
+                        }
+                    }
+                    else if (tokens[1] == "EndsWith")
+                    {
+                        foreach (var item in partyPeople)
+                        {
+                            if (predicateEndsWith(item))
+                            {
+                                nameToaAdd = item;
+                                times++;
+                            }
+                        }
+                    }
+                    else if (tokens[1] == "Length")
+                    {
+                        
+                        foreach (var item in partyPeople)
+                        {
+                            if (predicateNameLength(item))
+                            {
+                                nameToaAdd = item;
+                                times++;
+                            }
+                        }
+                       
+                    }
+                    for (int i = 0; i < times; i++)
+                    {
+                        partyPeople.Add(nameToaAdd);
+                    }
+                }
+
+            }
+
+            if (partyPeople.Count != 0)
+            {
+                Console.WriteLine($"{string.Join(", ", partyPeople)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+            
+
+        }
+
+        
+    }
+}
