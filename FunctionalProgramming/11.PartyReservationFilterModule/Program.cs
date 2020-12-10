37ec567d10eac70a189df41afdd126fbcf686646
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-10 15:47:17 +0200

    12. TriFunction 11. Party Reservation Filter Module

--- FunctionalProgramming/11.PartyReservationFilterModule/Program.cs
+++ FunctionalProgramming/11.PartyReservationFilterModule/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11.PartyReservationFilterModule
 {
@@ -6,7 +8,54 @@ namespace _11.PartyReservationFilterModule
     {
         static void Main(string[] args)
         {
-            
+            List<string> partyPeople = Console.ReadLine().Split().ToList();
+            List<string> filters = new List<string>();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Print")
+                {
+                    break;
+                }
+                string[] tokens = command.Split(";").ToArray();
+                if (tokens[0] == "Add filter")
+                {
+                    filters.Add($"{tokens[1]};{tokens[2]}");
+                }
+                else if (tokens[0] == "Remove filter")
+                {
+                    filters.Remove($"{tokens[1]};{tokens[2]}");
+                }
+            }
+
+            for (int i = 0; i < filters.Count; i++)
+            {
+                
+                string[] command = filters[i].Split(";").ToArray();
+                Func<string, bool> predicateStartsWith = x => !x.StartsWith(command[1]);
+                Func<string, bool> predicateEndsWith = x => !x.EndsWith(command[1]);
+                Func<string, bool> predicateNameLength = x => !(x.Length == int.Parse(command[1]));
+                Func<string, bool> predicateContains = x => !x.Contains(command[1]);
+                if (command[0] == "Starts with")
+                {
+                    partyPeople = partyPeople.Where(predicateStartsWith).ToList();
+                }
+                else if (command[0] == "Ends with")
+                {
+                    partyPeople = partyPeople.Where(predicateEndsWith).ToList();
+                }
+                else if (command[0] == "Length")
+                {
+                    partyPeople = partyPeople.Where(predicateNameLength).ToList();
+                }
+                else if (command[0] == "Contains")
+                {
+                    partyPeople = partyPeople.Where(predicateContains).ToList();
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(" ", partyPeople));
         }
     }
 }
---
+++ FunctionalProgramming/12.TriFunction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+            Func<string, int> namesInAscii = name => name.Select(c => (int)c).Sum();
+            //secondWay
+            //{
+            //    int sum = 0;
+            //    for (int i = 0; i < name.Length; i++)
+            //    {
+            //        sum += name[0];
+            //    }
+            //    return sum;
+            //};
+
+            Func<List<string>, int, Func<string, int>, string> res = (names, n, namesInAscii) =>
+            {
+                string name = null;
+                for (int i = 0; i < names.Count; i++)
+                {
+                    if (namesInAscii(names[i]) >= n)
+                    {
+                        name = names[i];
+                        break;
+                    }
+                }
+                return name;
+
+            };
+
+            Console.WriteLine(res(names, n, namesInAscii));
+        }
+    }
+}
