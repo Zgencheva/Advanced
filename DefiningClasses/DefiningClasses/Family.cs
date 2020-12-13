c6ba35ffc54f33dd9ae6b6083764b704a3a1ef1d
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-13 16:54:46 +0200

    DefineClassesEx

---
+++ DefiningClasses/DefiningClasses/Family.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Family
+    {
+
+        public List<Person> FamilyMembers { get; set; }
+
+        public Family()
+        {
+            this.FamilyMembers = new List<Person>();
+        }
+        public void AddMember(Person member)
+        {
+
+            
+            this.FamilyMembers.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = new Person(int.MinValue);
+
+            for (int i = 0; i < this.FamilyMembers.Count; i++)
+            {
+                if (FamilyMembers[i].Age > oldestMember.Age)
+                {
+                    oldestMember = FamilyMembers[i];
+                }
+            }
+
+            return oldestMember;
+        }
+    }
+}
--- DefiningClasses/DefiningClasses/Program.cs
+++ DefiningClasses/DefiningClasses/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DefiningClasses
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-           
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split().ToArray();
+                Person newPerson = new Person(tokens[0], int.Parse(tokens[1]));
+                people.Add(newPerson);
+
+            }
+
+            foreach (Person item in people.Where(x => x.Age > 30).OrderBy(y => y.Name))
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
         }
     }
 }
