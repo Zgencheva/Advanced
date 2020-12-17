a8084db5a3615551a9b5fe08a1c6ece8bb31ce4c
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-17 17:26:58 +0200

    CarSalesman

---
+++ DefiningClasses/CarSalesman/Car.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesman
+{
+    public class Car
+    {
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = 0;
+            this.Color = "n/a";
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.Model+':');
+            sb.AppendLine($"  {this.Engine.Model}:");
+            sb.AppendLine($"    Power: {this.Engine.Power}");
+            
+            if (this.Engine.Displacement == 0)
+            {
+                sb.AppendLine($"  Displacement: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"    Displacement: {this.Engine.Displacement}");
+            }
+            sb.AppendLine($"    Efficiency: {this.Engine.Efficiency}");
+            if (this.Weight == 0)
+            {
+                sb.AppendLine($"  Weight: n/a");
+            }
+            else
+            {
+                sb.AppendLine($"  Weight: {this.Weight}");
+            }
+            
+            sb.Append($"  Color: {this.Color}");
+            return sb.ToString(); 
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+        public string Color { get; set; }
+
+    }
+}
---
+++ DefiningClasses/CarSalesman/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace CarSalesman
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int enginesN = int.Parse(Console.ReadLine());
+            List<Engine> engines = new List<Engine>();
+            for (int i = 0; i < enginesN; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length == 4)
+                {
+                    Engine currentEngine = new Engine(tokens[0], int.Parse(tokens[1]), int.Parse(tokens[2]), tokens[3]);
+                    engines.Add(currentEngine);
+                }
+                else if (tokens.Length == 3)
+                {
+                    
+                    if (char.IsDigit(tokens[2][0]))
+                    {
+                        Engine currentEngine = new Engine(tokens[0], int.Parse(tokens[1]), int.Parse(tokens[2]), "n/a");
+                        engines.Add(currentEngine);
+                    }
+                    else
+                    {
+                        Engine currentEngine = new Engine(tokens[0], int.Parse(tokens[1]), 0, tokens[2]);
+                        engines.Add(currentEngine);
+                    }
+                }
+                else
+                {
+                    Engine currentEngine = new Engine(tokens[0], int.Parse(tokens[1]));
+                    engines.Add(currentEngine);
+                }
+                
+
+            }
+            int carsN = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < carsN; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (tokens.Length == 4)
+                {
+                    Car currentCar = new Car(tokens[0], engines.Where(x => x.Model == tokens[1]).FirstOrDefault(), int.Parse(tokens[2]), tokens[3]);
+                    cars.Add(currentCar);
+                }
+                else if (tokens.Length == 3)
+                {
+
+                    if (char.IsDigit(tokens[2][0]))
+                    {
+                        Car currentCar = new Car(tokens[0], engines.Where(x => x.Model == tokens[1]).FirstOrDefault(), int.Parse(tokens[2]), "n/a");
+                        cars.Add(currentCar);
+                    }
+                    else
+                    {
+                        Car currentCar = new Car(tokens[0], engines.Where(x => x.Model == tokens[1]).FirstOrDefault(),0, tokens[2]);
+                        cars.Add(currentCar);
+                    }
+                }
+                else
+                {
+                    Car currentCar = new Car(tokens[0], engines.Where(x => x.Model == tokens[1]).FirstOrDefault());
+                    cars.Add(currentCar);
+                }
+
+            }
+
+            foreach (Car item in cars)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+        }
+    }
+}
