5a374e388243ef44d2668f23602e08d0408f0a66
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-24 22:13:22 +0200

    GenericsLab

---
+++ Generics/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+
+       public static T[] Create<T>(int length, T item)
+        {
+            T[] current = new T[length];
+            for (int i = 0; i < current.Length; i++)
+            {
+                current[i] = item;
+            }
+
+            return current;
+        }
+    }
+}
---
+++ Generics/Generics/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> MyStack;
+        public int Count => this.MyStack.Count;
+
+        public Box()
+        {
+            this.MyStack = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            MyStack.Push(element);
+        }
+        public T Remove()
+        {
+           return  this.MyStack.Pop();
+        }
+
+    }
+}
