70623776b9d0ff0d52cd149ef6ffbaec99c76761
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-25 11:19:19 +0200

    GenericScale

---
+++ Generics/GenericScale/EqualityScale.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace GenericScale
+{
+    class EqualityScale<T>
+    {
+
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public bool AreEqual()
+        {
+            bool areEguql = false;
+            if (this.Left.Equals(this.Right))
+            {
+                areEguql = true;
+            }
+
+            return areEguql;
+        }
+
+
+    }
+}
