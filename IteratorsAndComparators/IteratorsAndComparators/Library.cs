d4c258661b19872f3f5ac1ebdf4c4267bc1eb0fc
Zornitsa Gencheva <zornitsa.sertova@gmail.com>
2020-12-26 16:02:45 +0200

    IteratorsAndComparatorsLibrary

---
+++ IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    
+
+    public class Library : IEnumerable<Book>
+    {
+        public List<Book> Books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            this.Books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return Books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
